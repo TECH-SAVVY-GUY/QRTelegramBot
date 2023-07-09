f58050ba465add96effe33904b86f2f8904ae451
Soham Datta <83786816+TECH-SAVVY-GUY@users.noreply.github.com>
2023-07-10 01:10:29 +0530

    Create `script.js`

---
+++ static/script.js
@@ -0,0 +1,20 @@
+Telegram.WebApp.ready()
+Telegram.WebApp.expand()
+
+function handleQRTextReceived(event) {
+  var qrText = event.data;
+  fetch('/qrCode-response', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ qr: qrText, initData: window.Telegram.WebApp.initData })
+  });
+
+  Telegram.WebApp.offEvent('qrTextReceived', handleQRTextReceived);
+
+  setTimeout(Telegram.WebApp.closeScanQrPopup(), 1000);
+}
+
+Telegram.WebApp.onEvent('qrTextReceived', handleQRTextReceived);
